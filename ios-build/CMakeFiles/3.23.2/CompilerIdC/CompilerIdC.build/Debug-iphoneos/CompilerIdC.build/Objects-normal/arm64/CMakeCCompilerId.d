3eee1f32f7bb5faf495268d8834f7ae7df462bcb
montaluser <2521754303@qq.com>
2022-07-20 11:05:04 +0800

    Cocos2d-x Sample v0.1

---
+++ ios-build/CMakeFiles/3.23.2/CompilerIdC/CompilerIdC.build/Debug-iphoneos/CompilerIdC.build/Objects-normal/arm64/CMakeCCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/chenshixin/Desktop/cocos-cpp-sample/ios-build/CMakeFiles/3.23.2/CompilerIdC/CMakeCCompilerId.c
---
+++ mac-build/CMakeFiles/3.23.2/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/x86_64/CMakeCCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/chenshixin/Desktop/cocos-cpp-sample/mac-build/CMakeFiles/3.23.2/CompilerIdC/CMakeCCompilerId.c
